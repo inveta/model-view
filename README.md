c534e3a0a7817ed9f6546c440b6d0a9317590bd6
XOSG <2195868682@qq.com>
2021-11-19 08:39:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Model View
+
+基于 Zero Overhead 原则的草量级 3D 模型渲染组件，在线演示：https://pqo.gitee.io/model-view/demo/
+
+```
+<script src="model-view.js"></script>
+<canvas is="model-view" src="path/to/model.bin" width="500" height="500"></canvas>
+```
+
+| model-view           | 大小   | 内容                   |
+| -------------------- | ------ | ---------------------- |
+| model-view.js        | 5.0 KB | 源文件：含注释和空白符 |
+| model-view.min.js    | 3.5 KB | 代码丑化工具编译后     |
+| model-view.min.js.gz | 1.5 KB | Gzip 压缩后：http 传输 |
+
+## 动机
+
+市面上的 WebGL 库为了支持 3D 模型的各种属性，文件体积异常庞大，动辄 1M 以上，但很多时候用户只需要简单直观地展示一下模型，并不关心图形学中乱七八糟的功能，也就是所谓的“零负担原则”（zero overhead principle），因此本项目诞生，本项目选取了三维模型中最关键的几个属性，其他的一概不支持！因此得以让库文件保持几 KB，用最小的开销渲染尽可能多的信息：本库不支持市面上任何的三维模型格式，取而代之的是自定义的，可直接传入 WebGL 缓冲区的二进制格式。
+
+- 轻巧组件：渲染所占资源极小；使用了 html 组件化
+- 聚焦透视：自动聚焦到物体的包围盒，并且透视投影
+- 多种材质：支持给三角面分组，每组分配不同的颜色
+- 扁平着色：模拟一束来自视点的平行光线
+- 简单交互：支持鼠标操作，围绕物体旋转缩放
+
+## 自定义原生格式
+
+| 二进制格式  | 类型      | 长度         | 作用                       |
+| ----------- | --------- | ------------ | -------------------------- |
+| json length | Uint 32   | 4 字节       | 定义了下一块的长度         |
+| json        | JSON 文本 | 由上一块决定 | json 文本 ascii            |
+| 顶点坐标    | 数组      | 由上一块决定 | 每个顶点由 3 个 float 组成 |
+| 三角索引    | 数组      | 余下的长度   | 索引的数量由上上块决定     |
+
+## JSON 格式
+
+| JSON 格式               | 类型        | 作用                 |
+| ----------------------- | ----------- | -------------------- |
+| position_length         | 整数        | 顶点数组的长度，字节 |
+| MIN、MAX                | vector3     | AABB 包围盒的端点    |
+| extensions              | string 列表 | WebGL 的扩展功能     |
+| groups                  | 字典列表    | 三角面的分组         |
+| groups -> color         | vector4     | 分组的颜色           |
+| groups -> indexCount    | 整数        | 分组的长度           |
+| groups -> componentType | WebGL 类型  | 索引的类型           |
+| groups -> offset        | 整数        | 分组的偏移值，字节   |
---
+++ demo/glb2bin.js
@@ -0,0 +1,82 @@
+const file = "monkey.glb";
+
+const fs = require("fs");
+
+let buffer = fs.readFileSync(file).buffer;
+
+const jsonLength = new Uint32Array(buffer.slice(12, 12 + 4))[0];
+
+const { meshes, materials, accessors, bufferViews } = JSON.parse(
+  new TextDecoder().decode(buffer.slice(20, 20 + jsonLength))
+);
+
+const groups = [];
+
+buffer = buffer.slice(20 + jsonLength + 8);
+
+const position = [];
+const indices = [];
+groups[-1] = {
+  offset: 0,
+  count: 0,
+  byteLength: 0,
+  totalVertexCount: 0,
+};
+let MIN = Array(3).fill(Infinity);
+let MAX = Array(3).fill(-Infinity);
+
+const extensions = new Set(["OES_standard_derivatives"]);
+
+meshes[0].primitives.forEach((primitive, i) => {
+  let color = [1, 1, 1];
+  try {
+    color = materials[primitive.material].pbrMetallicRoughness.baseColorFactor.slice(0, 3);
+  } catch (err) {}
+
+  let vertex = accessors[primitive.attributes.POSITION];
+  MIN = MIN.map((a, i) => Math.min(a, vertex.min[i]));
+  MAX = MAX.map((a, i) => Math.max(a, vertex.max[i]));
+  const { byteLength, byteOffset } = bufferViews[vertex.bufferView];
+  position.push(buffer.slice(byteOffset, byteOffset + byteLength));
+  const index = accessors[primitive.indices];
+  let bufferView = bufferViews[index.bufferView];
+  const UintArray = { 5121: Uint8Array, 5123: Uint16Array, 5125: Uint32Array }[index.componentType];
+  if (UintArray === Uint32Array) extensions.add("OES_element_index_uint");
+  indices.push(
+    new UintArray(
+      buffer.slice(bufferView.byteOffset, bufferView.byteOffset + bufferView.byteLength)
+    ).map((x) => x + groups[i - 1].totalVertexCount)
+  );
+  groups[i] = {
+    color,
+    byteLength: bufferView.byteLength,
+    indexCount: index.count,
+    totalVertexCount: groups[i - 1].totalVertexCount + vertex.count,
+    componentType: index.componentType,
+    offset: groups[i - 1].offset + groups[i - 1].byteLength,
+  };
+});
+
+delete groups[-1];
+groups.forEach((color) => {
+  delete color.byteLength;
+  delete color.totalVertexCount;
+});
+
+const json = JSON.stringify({
+  groups,
+  MIN,
+  MAX,
+  position_length: position.reduce((a, b) => a + b.byteLength, 0),
+  extensions: [...extensions],
+});
+
+const buffers = [
+  new Uint32Array([json.length]).buffer,
+  json,
+  ...position,
+  ...indices.map((a) => a.buffer),
+].map((a) => Buffer.from(a));
+
+fs.writeFileSync(file.replace(".glb", ".bin"), Buffer.concat(buffers));
+// fs.appendFileSync()
